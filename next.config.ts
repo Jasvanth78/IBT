de21e2ba8c40a4193b18776652a6ec2f5aa32485
Jasvanth78 <mailtojasvanth88@gmail.com>
2026-05-27 15:39:33 +0530

    fix deploy issue -1

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
 const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  // GitHub Pages usually hosts from https://Jasvanth78.github.io/IBT/
-  // We only apply basePath in production/CI to avoid breaking local dev
+  // GitHub Pages only
   basePath: isGithubActions ? '/IBT' : '',
+
   images: {
     unoptimized: true,
     remotePatterns: [
@@ -25,8 +23,8 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
   allowedDevOrigins: ['10.248.33.25', 'localhost:3000'],
 };
 
 export default nextConfig;
-
